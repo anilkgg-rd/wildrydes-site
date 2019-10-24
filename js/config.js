0c50f6b36ef050217fce257fb99c6098c3a50ee2
anilkgg-rd <anilkggv3.0@gmail.com>
2019-10-24 16:09:58 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Yx9j06jwF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2cf85h5gruvsr1gi99hrueusmb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
